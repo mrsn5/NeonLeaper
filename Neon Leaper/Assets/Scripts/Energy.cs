d0fb6e2cf26a2a41727d99fbf27fea3e68876bd2
AnneManzhura <manzhurik@gmail.com>
2018-06-25 16:57:39 +0300

    Crystals, energy level, simple menu

---
+++ Neon Leaper/Assets/Scripts/Energy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Energy : MonoBehaviour {
+	
+    public static Energy current=null;
+    public Slider slider;
+
+	private float valueToBecome = 100;
+		
+		
+    void Awake() {
+        current = this;
+    }
+    
+	void Update () {
+		slider.value=Mathf.MoveTowards(slider.value, valueToBecome, 0.5f);    
+	}
+
+	public void setValue(float val)
+	{
+		slider.value = val;
+		valueToBecome = val;
+	}
+	
+	public void setValueSlowly(float value)
+	{
+		valueToBecome = value;
+	}
+
+	public float energyLevel()
+	{
+		return slider.value;
+	}
+	
+}
--- Neon Leaper/Assets/Scripts/LevelController.cs
+++ Neon Leaper/Assets/Scripts/LevelController.cs
@@ -4,17 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-
     public static LevelController current;
     Vector3 startingPosition;
     [SerializeField]
     GameObject playerPrefab;
 
+    public int defaultCrystals;
+   
+    private int crystals;
+    public Text crystalsText;
+
     void Awake()
     {
         current = this;
     }
-
+    
+    void Update()
+    {
+        crystalsText.text = string.Format("{0}/{1}", crystals, defaultCrystals);
+    }
+    
     public void setStartPosition(Vector3 pos)
     {
         this.startingPosition = pos;
@@ -25,5 +34,10 @@ public class LevelController : MonoBehaviour
         player.transform.position = startingPosition;
     }
 
+    public void addCrystal()
+    {
+        crystals++;
+    }
+    
 
 }
