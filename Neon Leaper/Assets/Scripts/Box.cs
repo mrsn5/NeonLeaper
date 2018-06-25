964a36340e5d1505df7aa79b3f6d84c27db6a784
San Nguyen <mr.sn5.kma@gmail.com>
2018-06-25 15:28:59 +0300

    boxes in timeline added

---
+++ Neon Leaper/Assets/Scripts/Box.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    [SerializeField]
+    private List<int> states;
+
+    // TODO
+    public Vector3[] positions = new Vector3[3];
+
+    private void Start()
+    {
+        LevelController.current.AddBox(this);
+        foreach (int i in states)
+            positions[i] = transform.position;
+    }
+
+    private void Update () {
+        for (int state = LevelController.current.GetState(); state < 3; state++)
+            positions[state] = transform.position;
+	}
+
+    public bool HasState(int state)
+    {
+        if (states.Contains(state)) return true;
+        return false;
+    }
+
+    public void SetPosition(int state)
+    {
+        transform.position = positions[state];
+    }
+}
--- Neon Leaper/Assets/Scripts/LevelController.cs
+++ Neon Leaper/Assets/Scripts/LevelController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -9,10 +11,22 @@ public class LevelController : MonoBehaviour
     Vector3 startingPosition;
     [SerializeField]
     GameObject playerPrefab;
+    [SerializeField]
+    GameObject boxPrefab;
+
+    List<Box> boxes = new List<Box>();
+    [SerializeField]
+    private Slider slider;
 
     void Awake()
     {
+        slider.value = 1f;
         current = this;
+    }
+
+    private void Start()
+    {
+        SetBoxes();     
     }
 
     public void setStartPosition(Vector3 pos)
@@ -23,6 +37,35 @@ public class LevelController : MonoBehaviour
     public void Respawn(Player player)
     {
         player.transform.position = startingPosition;
+    }
+
+    public void AddBox(Box box) 
+    {
+        boxes.Add(box);
+    }
+
+    public int GetState()
+    {
+        return (int)slider.value;
+    }
+
+
+    private void SetBoxes()
+    {
+        foreach (Box box in boxes)
+        {
+            box.gameObject.SetActive(false);
+            if (box.HasState(GetState()))
+            {
+                box.gameObject.SetActive(true);
+                box.SetPosition(GetState());
+            }
+        }
+    }
+
+    public void SliderChanged()
+    {
+        SetBoxes();
     }
 
 
